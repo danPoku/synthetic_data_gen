dc1c099a95e80f9b80a0e90f702ba10bebf38cc9
danPoku <dan.gyinaye@gmail.com>
2025-04-12 22:46:43 +0000

    Update 'M' in df_diag.resample('M') to 'ME'

--- toolkit/utils.py
+++ toolkit/utils.py
@@ -10,7 +10,7 @@ def preview_data(df, n=10):
 def plot_monthly_trends(df, diagnosis):
     df_diag = df[df['primary_diagnosis'] == diagnosis]
     df_diag.index = pd.to_datetime(df_diag['datetime'])
-    monthly_cases = df_diag.resample('M').sum()['number_of_cases']
+    monthly_cases = df_diag.resample('ME').sum()['number_of_cases']
     monthly_cases.plot(title=f'Monthly Trend for {diagnosis}')
     plt.xlabel('Month')
     plt.ylabel('Cases')
