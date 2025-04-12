caa15e205b7c91fb28f26b7294052d993181ef03
danPoku <dan.gyinaye@gmail.com>
2025-04-12 22:52:09 +0000

    commit

--- build/lib/toolkit/core.py
+++ build/lib/toolkit/core.py
@@ -1,19 +1,19 @@
 import numpy as np
 import pandas as pd
-from .diseases import load_disease_definitions
-from .temporal import apply_temporal_factors
+from .diseases import disease_definitions
+from .temporal import temporal_factors
 
 class SyntheticDataGenerator:
     def __init__(self, location, start_date, end_date, seed=42):
         self.location = location
         self.dates = pd.date_range(start=start_date, end=end_date, freq='D')
         np.random.seed(seed)
-        self.diseases, self.seasonal_weights, self.trend_factors = load_disease_definitions()
+        self.diseases, self.seasonal_weights, self.trend_factors = disease_definitions()
 
     def simulate(self):
         data_records = []
         for date in self.dates:
-            day_factor = apply_temporal_factors(date)
+            day_factor = temporal_factors(date)
             month_index = date.month - 1
             year = date.year
 
