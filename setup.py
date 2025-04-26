9d0e6bb085d298e3314891e6fda9a6c8d96f57d1
danPoku <dan.gyinaye@gmail.com>
2025-04-26 23:08:15 +0000

    chore: version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='synthetic_data_toolkit',
-    version='0.1.0',
+    version='0.1.3',
     description='A modular toolkit for generating synthetic datasets for respiratory disease incidence in Accra, Ghana.',
     author='Dan Gyinaye Poku',
     author_email='dan.gyinaye@gmail.com',
